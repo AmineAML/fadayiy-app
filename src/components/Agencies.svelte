61a2d30dca98a8a979ace72a8f4df8c0658f4464
Amine AML <amin_aml@live.fr>
2021-12-31 18:00:58 +0100

    chore: display the logos as the API handle checking if they're valid, no checking on the app

--- src/components/Agencies.svelte
+++ src/components/Agencies.svelte
@@ -5,14 +5,22 @@
 
   export let agencies;
 
+  // const { agencies } = Astro.props;
+
   let showMessage = false;
 
+  let showFallbackImageFromBing = false;
+
   async function onImageDisplayError(agencyName) {
+    console.log("search url");
+
+    showFallbackImageFromBing = false;
+
     const query = `${agencyName} logo`;
 
     const queryImageUrl = gql`
       {
-        bingImageSearch(query: query) {
+        bingImageSearch(query: "${query}") {
           url
         }
       }
@@ -20,9 +28,28 @@
 
     const imageUrlRes = await request(fadayiyGraphqlApi, queryImageUrl);
 
-    const { image } = imageUrlRes;
+    console.log("url");
 
-    return `this.onerror=null;this.src='${image.url}';`;
+    console.log(imageUrlRes);
+
+    const { bingImageSearch } = imageUrlRes;
+
+    return `this.onerror=null;this.src='${bingImageSearch.url}';`;
+
+    // return image.url;
+
+    // return '';
+  }
+
+  function hideOnError() {
+    showFallbackImageFromBing = true;
+
+    // return 'this.onerror=null;this.style.display="none"';
+  }
+
+  async function triggerAsync(agencyName) {
+    console.log("trigger async function");
+    await onImageDisplayError(agencyName);
   }
 </script>
 
@@ -34,7 +61,11 @@
       class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading"
       style="color: #3c415e;"
     >
-      Agencies
+      Space agencies
+      <br />
+      <p class="text-xs italic">
+        (could include those involved in spaceflights)
+      </p>
     </h2>
   </div>
   <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
@@ -43,7 +74,7 @@
         class="w-full bg-gray-900 rounded-lg sahdow-lg p-12 flex flex-col justify-center items-center"
       >
         <div class="mb-8">
-          <img
+          <!-- <img
             class="object-center object-cover rounded-full h-36 w-36"
             src={agency.name.toLowerCase() == "general electric"
               ? "https://logo.clearbit.com/ge.com"
@@ -57,25 +88,46 @@
               : agency.image_url}
             alt={agency.name + "'s logo"}
             onerror={onImageDisplayError(agency.name)}
+          /> -->
+          <!-- {#await onImageDisplayError(agency.name)}
+            loading
+          {:then url} 
+          <img
+          class="object-center object-cover rounded-full h-36 w-36"
+          src={agency.name.toLowerCase() == "general electric"
+            ? "https://logo.clearbit.com/ge.com"
+            : agency.info_url
+            ? "https://logo.clearbit.com/" +
+              agency.info_url
+                .replace(new RegExp("https://", "g"), "")
+                .replace(new RegExp("http://", "g"), "")
+                .replace(new RegExp("www.", "g"), "")
+                .split("/")[0]
+            : agency.image_url}
+          alt={agency.name + "'s logo"}
+          onerror="this.onerror=null;this.src='${url}'"
+        />
+          {/await} -->
+          <img
+            class="object-center object-cover rounded-full h-36 w-36"
+            src={agency.logo_url}
+            alt={agency.name + "'s logo"}
           />
-          {#if showMessage}
-            <p />
-          {/if}
         </div>
         <div class="text-center">
           <p class="text-xl text-white font-bold mb-2">
             {agency.name + " (" + agency.abbrev + ")"}
           </p>
-          <a href="/" class="btn btn-outline font-normal text-base text-white"
+          <a
+            href="/agencies/{agency.name.replace(
+              new RegExp(' ', 'g'),
+              '_'
+            )}_{agency.id}"
+            class="btn btn-outline font-normal text-base text-white"
             >Learn more</a
           >
         </div>
       </div>
     {/each}
   </div>
-  <a
-    class="btn btn-wide btn-lg bg-transparent mx-auto hover:bg-gray-400"
-    style="color: #3c415e;"
-    href="/">Show more</a
-  >
 </section>
